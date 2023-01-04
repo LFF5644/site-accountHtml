9b46cee2c07bdbe9eaee2043a7353e3e4679409d
LFF <meerschweinchen5644@gmail.com>
2023-01-04 14:18:23 +0100

    device list added

--- app_formSubmit.js
+++ app_formSubmit.js
@@ -1,3 +1,5 @@
+const ESC=escape;
+const unESC=unescape;
 
 function findActiveRadio(list){
 	let radio="";
@@ -17,6 +19,18 @@ function callApi(args){
 		.then(res=>res.text())
 		.then(res=>HandleServerResponse(res,args.want))
 }
+function ActionSubmit(){
+	const form=document.getElementById("form_main");
+	const mode=findActiveRadio(form.radio_action)=="register"?"createAccount":"createToken";
+	callApi({
+		want:mode,
+		username:form.username.value,
+		nickname:mode=="createAccount"?form.nickname.value:undefined,
+		password:form.password.value,
+		deviceName:form.deviceName.value,
+		token:null,
+	});
+}
 function HandleServerResponse(serverResponse,clientAction){
 	try{serverResponse=JSON.parse(serverResponse)}
 	catch(e){
@@ -26,42 +40,35 @@ function HandleServerResponse(serverResponse,clientAction){
 	if(serverResponse.code=="ok"){
 		if(!serverResponse.data.token){throw new Error("Kein Token empfangen!")}
 		
-		const token=escape(serverResponse.data.token);
-		document.cookie="token="+token+";path=/;maxAge=9999999;";
+		const token=ESC(serverResponse.data.token);
+		document.cookie=`token=${token};expires=Fri, 01 Jan 2100 00:00:00 GMT;path=/`
+		document.cookie=`deviceName=${document.getElementById("form_main").deviceName.value};expires=Fri, 01 Jan 2100 00:00:00 GMT;path=/`
 
-		alert("Anmelden/Regestrieren Erfolgreich!\n\nSie werden auf die startseite Weitergeleitet");
-		location.href="/";
+		alert((clientAction=="createAccount"?"Regestrieren":"Anmelden")+" Erfolgreich!\n\nÜbersicht wird geladen...");
+		location.reload();
 		return true;
 	}
 	else if(serverResponse.errormsg){
-		alert("Info vom Server:\n\n"+serverResponse.errormsg);
+		alert("Info vom Server:\n"+serverResponse.errormsg);
 	}
-}
-function ActionSubmit(){
-	const form=document.getElementById("form_main");
-	const mode=findActiveRadio(form.radio_action)=="register"?"createAccount":"createToken";
-	callApi({
-		want:mode,
-		username:form.username.value,
-		nickname:mode=="register"?form.nickname.value:undefined,
-		password:form.password.value,
-		token:null,
-		//debug:1,
-	});
 }
 function ActionChangeMode(){
 	const form=document.getElementById("form_main");
 	const button_submit=document.getElementById("button_submit");
 	const p_nickname=document.getElementById("p_nickname");
+	const p_deviceName=document.getElementById("p_deviceName");
 	const mode=findActiveRadio(form.radio_action);
 
 	if(mode=="login"){
 		button_submit.innerText="Anmelden";
 		p_nickname.className="hidden";
+		form.nickname.required=false;
 	}
 	else if(mode=="register"){
 		button_submit.innerText="Regestrieren";
 		p_nickname.className="";
+		form.nickname.required=true;
+
 	}
 	else{throw new Error("mode not allowed!");}
 }
---
+++ app_showAccount.js
@@ -0,0 +1,38 @@
+
+function callApi(args,data){
+	fetch("/server/account/account.api",{
+		method:"post",
+		headers:{"Content-Type":"application/json"},
+		body:JSON.stringify(args),
+	})
+		.then(res=>res.text())
+		.then(res=>HandleServerResponse(res,args.want,data))
+}
+function LogoutDevice(tokenCreated){
+	this.onclick=null;
+	callApi({
+		want:"LogoutDevice",
+		tokenCreated,
+	},{
+		removeElementById:"device_"+tokenCreated,
+	});
+}
+function HandleServerResponse(serverResponse,clientAction,args){
+	try{serverResponse=JSON.parse(serverResponse)}
+	catch(e){
+		alert("INTERNER SERVER FEHLER:\nBitte gebe Lando Bescheid das die variable serverResponse nicht in json umgewandelt werden konte!\n\nDie Action wird mit einem KRITISCHEN fehler abgebrochen\nDanke für ihr verständnis!\nGrüße vom Programmierer Lando!");
+		throw new Error("json cant parse");
+	}
+	if(serverResponse.code=="ok"){
+		if(clientAction=="LogoutDevice"){
+			const e=document.getElementById(args.removeElementById);
+			if(e.className.includes("thisDevice")){
+				location.reload();
+			}
+			e.remove();
+		}
+	}else{
+		if(serverResponse.errormsg){alert("Info vom Server:\n"+serverResponse.errormsg)};
+	}
+	console.log(serverResponse);
+}
